f33ff19072a523d894625de6d9cb9e17718ee910
Matt Patrick <hazmat345@gmail.com>
2020-12-19 23:07:04 -0500

    Adding plugin skeleton

---
+++ {{cookiecutter.plugin_name}}/setup.py
@@ -0,0 +1,36 @@
+import re
+
+from setuptools import setup
+
+
+def find_version(version_file):
+    version_line = open(version_file, "rt").read()
+    match_object = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_line, re.M)
+
+    if not match_object:
+        raise RuntimeError("Unable to find version string in %s" % version_file)
+
+    return match_object.group(1)
+
+
+setup(
+    name="{{ cookiecutter.plugin_name }}",
+    version=find_version("{{ cookiecutter.package_name }}/__main__.py"),
+    description="{{ cookiecutter.description }}",
+    url="{{ cookiecutter.url }}",
+    author="{{ cookiecutter.author }}",
+    author_email=" ",
+    license="{{ cookiecutter.license }}",
+    packages=["{{ cookiecutter.package_name }}"],
+    install_requires=["brewtils"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
---
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.package_name}}/__main__.py
@@ -0,0 +1,22 @@
+from brewtils import command, parameter, system, Plugin
+
+from __version__ import __version__
+
+
+@system
+class Client:
+    pass
+
+
+def main():
+    plugin = Plugin(
+        name="{{ cookiecutter.plugin_name }}",
+        version=__version__,
+        description="{{ cookiecutter.description }}",
+    )
+    plugin.client = Client()
+    plugin.run()
+
+
+if __name__ == "__main__":
+    main()
